324c4e626c00cb66bfe425cbdedd6db86bcb7983
Alan-Kuan <kuanhaoan@gmail.com>
2022-09-12 10:35:11 +0800

    chore: select least-costing plan for the DB

--- setup.sh
+++ setup.sh
@@ -123,6 +123,12 @@ if [ "$SERVER_NUM" -eq 0 ]; then
         --resource-group $RESOURCE_GROUP \
         --server $SQL_SERVER_NAME \
         --name $SQL_SERVER_DB_NAME \
+        --edition GeneralPurpose \
+        --family Gen5 \
+        --capacity 1 \
+        --compute-model Serverless \
+        --max-size 1GB \
+        --auto-pause-delay 60 \
         --backup-storage-redundancy Geo \
         --output none
 fi
